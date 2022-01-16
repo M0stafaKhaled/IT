3f4e6fcab71107c0abdbf67d0202de3e7fce7711
M0stafaKhaled <mkostafa79@gmail.com>
2022-01-17 01:03:24 +0200

    final

--- inex.js
+++ inex.js
@@ -1,8 +1,17 @@
 const bar = document.querySelector('.menu-bar');
 bar.addEventListener("click", ()=>{
 
-    bar.classList.toggle('is-active');
+    bar.classList.toggle('is-active' );
+    bar.classList.toggle('rr' );
+  
+    // bar.classList.toggle('menu-change' );
+    document.querySelector('.nav-menu').classList.toggle('bd')
 })
+
+document.querySelector('.nav-menu').addEventListener("click" ,function(){
+    document.querySelector('.nav-menu').classList.toggle('bd')
+    bar.classList.remove('is-active' );
+});
 
 var nav = document.getElementById('nav');
 
