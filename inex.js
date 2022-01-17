736ef01af64d46d48e24d7b2e41efd69c1d7171d
M0stafaKhaled <mkostafa79@gmail.com>
2022-01-17 06:29:05 +0200

    m

--- inex.js
+++ inex.js
@@ -1,27 +1,45 @@
-const bar = document.querySelector('.menu-bar');
-bar.addEventListener("click", ()=>{
+// const bar = document.querySelector('.menu-bar');
+// bar.addEventListener("click", ()=>{
 
-    bar.classList.toggle('is-active' );
-    bar.classList.toggle('rr' );
+//     bar.classList.toggle('is-active' );
+//     bar.classList.toggle('rr' );
   
-    // bar.classList.toggle('menu-change' );
-    document.querySelector('.nav-menu').classList.toggle('bd')
-})
+//     // bar.classList.toggle('menu-change' );
+//     document.querySelector('.nav-menu').classList.toggle('bd')
+// })
 
-document.querySelector('.nav-menu').addEventListener("click" ,function(){
-    document.querySelector('.nav-menu').classList.toggle('bd')
-    bar.classList.remove('is-active' );
-});
+// document.querySelector('.nav-menu').addEventListener("click" ,function(){
+//     document.querySelector('.nav-menu').classList.toggle('bd')
+//     bar.classList.remove('is-active' );
+// });
 
-var nav = document.getElementById('nav');
+// var nav = document.getElementById('nav');
 
-    window.addEventListener("scroll" , function(){
+//     window.addEventListener("scroll" , function(){
 
 
                 
-        if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) 
-            nav.style.padding ="30px";
-            else
-            nav.style.padding ="10px"
+//         if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) 
+//             nav.style.padding ="30px";
+//             else
+//             nav.style.padding ="10px"
         
+//     });
+
+
+
+    
+    const d = document.querySelector(".links");
+    d.addEventListener("click" , function(){
+                // document.querySelector(".menu-icon").style.display ="none" marginLeft
+                // d.style.margin = "0 -400px" ; 
+                // d.style.top = "70px"
+                // d.style.left = "-100%"
+                // d.style.left = "-100%"
+                if(window.innerWidth <800)
+                location.reload();
+               
+                console.log("mostaf")
+                
+
     });
